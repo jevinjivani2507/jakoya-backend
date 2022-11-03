06232cf3fc5ed5720257a5a24ae2a3ebb00ccd33
Jevin Jivani <jivani.jevin25@gmail.com>
2022-11-03 23:21:25 +0530

    Database connected

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 import express from "express";
-import { APP_PORT } from "./config";
+import mongoose from "mongoose";
+import { APP_PORT, DB_URL } from "./config";
 
 const app = express();
 
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
 app.listen(APP_PORT, () => {
-    console.log(`Server is running on port ${APP_PORT}`);
+  console.log(`Server is running on port ${APP_PORT}`);
 });
